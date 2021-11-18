2890881cda26ba2d0422f2792c097f1185de6b22
Brandon <noneya67@yahoo.com>
2021-11-18 02:20:49 -0500

    close to finish basic mobile styling

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import React, { useState } from 'react'
 export default function Home() {
   const [heroImg, setHeroImg] = useState(0)
 
+
+
   function changeImg() {
     if (heroImg === 0) {
       return 'hero-1'
@@ -14,6 +16,18 @@ export default function Home() {
     if (heroImg === 2) {
       return 'hero-3'
     }
+
+  function leftClick() {
+    if (heroImg != 0) {
+      setHeroImg(heroImg - 1)
+    }
+  }
+
+  function rightClick() {
+    if (heroImg != 2) {
+      setHeroImg(heroImg + 1)
+    }
+  }
   }
   return (
     <div className='mainContainer'>
@@ -23,13 +37,13 @@ export default function Home() {
           </svg></div>
         <div className='title'>room</div>
         <div className='heroNav'>
-          <button className='leftButton'>
-            <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg">
+          <button className='leftButton' >
+            <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 -10 10 40">
               <path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fill-rule="evenodd" />
             </svg>
           </button>
-          <button className='rightButton'>
-            <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg">
+          <button className='rightButton' >
+            <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 -10 10 40">
               <path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fill-rule="evenodd" />
             </svg>
           </button>
@@ -42,7 +56,7 @@ export default function Home() {
         in bringing your vision to life. Create a room in your own style with our collection
         and make your property a reflection of you and what you love. 
         </div>
-        <div className='shopButton'>SHOP NOW<span><svg width="40" height="12" xmlns="http://www.w3.org/2000/svg">
+        <div className='shopButton'>SHOP NOW<span><svg width="40" height="12" xmlns="http://www.w3.org/2000/svg" className='arrowIcon'>
           <path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero" />
           </svg></span> 
         </div>
