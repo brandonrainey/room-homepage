fccb93c2193edaf19c6526892bab20427aad61d4
Brandon <noneya67@yahoo.com>
2021-11-19 23:54:38 -0500

    fixed jsx syntax preventing netlify deploy

--- pages/index.js
+++ pages/index.js
@@ -64,19 +64,19 @@ export default function Home() {
       
       <div className={`cell-1 ${textData[heroImg].img}`}>
         <div className='navIcon' onClick={() => setMenuToggle(true)}><svg width="20" height="14" xmlns="http://www.w3.org/2000/svg">
-          <path d="M20 12v2H0v-2h20zm0-6v2H0V6h20zm0-6v2H0V0h20z" fill="#FFF" fill-rule="evenodd" />
+          <path d="M20 12v2H0v-2h20zm0-6v2H0V6h20zm0-6v2H0V0h20z" fill="#FFF" fillRule="evenodd" />
           </svg></div>
         <div className='title'>room</div>
         <DesktopMenu />
         <div className='heroNavMobile'>
           <button className='leftButton' onClick={() => leftClick()}>
             <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 -10 10 40">
-              <path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fill-rule="evenodd" />
+              <path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRrule="evenodd" />
             </svg>
           </button>
           <button className='rightButton' onClick={() => rightClick()}>
             <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg" viewBox="0 -10 10 40">
-              <path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fill-rule="evenodd" />
+              <path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd" />
             </svg>
           </button>
         </div>
@@ -86,18 +86,18 @@ export default function Home() {
         <div className='description'>{textData[heroImg].description} 
         </div>
         <div className='shopButton'>SHOP NOW<span><svg width="40" height="12" xmlns="http://www.w3.org/2000/svg" className='arrowIcon'>
-          <path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fill-rule="nonzero" />
+          <path d="M34.05 0l5.481 5.527h.008v.008L40 6l-.461.465v.063l-.062-.001L34.049 12l-.662-.668 4.765-4.805H0v-1h38.206l-4.82-4.86L34.05 0z" fill="#000" fillRule="nonzero" />
           </svg></span> 
         </div>
         <div className='heroNavDesktop'>
           <button className='leftButton' onClick={() => leftClick()}>
             <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg" >
-              <path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fill-rule="evenodd" />
+              <path d="M13 0L1 12l12 12" stroke="#FFF" fill="none" fillRule="evenodd" />
             </svg>
           </button>
           <button className='rightButton' onClick={() => rightClick()}>
             <svg width="14" height="24" xmlns="http://www.w3.org/2000/svg" >
-              <path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fill-rule="evenodd" />
+              <path d="M1 0l12 12L1 24" stroke="#FFF" fill="none" fillRule="evenodd" />
             </svg>
           </button>
         </div>
