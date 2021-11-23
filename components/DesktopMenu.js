9085eee990b11c67f6111667f4c710ae0242ca76
Brandon <noneya67@yahoo.com>
2021-11-23 01:45:20 -0500

    added mouse pointers and selected states for nav items

--- components/DesktopMenu.js
+++ components/DesktopMenu.js
@@ -1,12 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function DesktopMenu() {
+    const [navItem, setNavItem] = useState()
+
+    function handleHomeClick() {
+        if (navItem === 0) {
+            setNavItem('')
+        } else {
+            setNavItem(0)
+        }
+    }
+
+    function handleShopClick() {
+        if (navItem === 1) {
+            setNavItem('')
+        } else {
+            setNavItem(1)
+        }
+    }
+
+    function handleAboutClick() {
+        if (navItem === 2) {
+            setNavItem('')
+        } else {
+            setNavItem(2)
+        }
+    }
+
+    function handleContactClick() {
+        if (navItem === 3) {
+            setNavItem('')
+        } else {
+            setNavItem(3)
+        }
+    }
+    
     return (
-        <div className='desktopMenu'> 
-            <a className='desktopMenuItem'>home</a>
-            <a className='desktopMenuItem'>shop</a>
-            <a className='desktopMenuItem'>about</a>
-            <a className='desktopMenuItem'>contact</a>
+        <div className='desktopMenu'>
+            <div className='navWrapper'>
+                <a className='desktopMenuItem' onClick={handleHomeClick}>home</a>
+                <div className={`${navItem === 0 ? 'homeSelected' : ''}`}></div>
+            </div> 
+            <div className='navWrapper'>
+                <a className='desktopMenuItem' onClick={handleShopClick}>shop</a>
+                <div className={`${navItem === 1 ? 'shopSelected' : ''}`}></div>
+            </div>
+            <div className='navWrapper'>
+                <a className='desktopMenuItem' onClick={handleAboutClick}>about</a>
+                <div className={`${navItem === 2 ? 'aboutSelected' : ''}`}></div>
+            </div>
+            <div className='navWrapper'>
+                <a className='desktopMenuItem' onClick={handleContactClick}>contact</a>
+                <div className={`${navItem === 3 ? 'contactSelected' : ''}`}></div>
+            </div>
+            
         </div>
     )
 }
